b38f54280d3f4e0d25a832a40d8cb6bfa6ecc69c
Michael Wolfendale <michael.wolfendale@gmail.com>
2018-01-13 18:30:58 +0000

    Fix tests

--- src/test/scala/wolfendale/scalacheck/regexp/RegexpGenSpec.scala
+++ src/test/scala/wolfendale/scalacheck/regexp/RegexpGenSpec.scala
@@ -4,7 +4,7 @@ import org.scalacheck.{Arbitrary, Gen, Shrink}
 import org.scalatest.prop.PropertyChecks
 import org.scalatest.{MustMatchers, WordSpec}
 
-class RegexGenSpec extends WordSpec with MustMatchers with PropertyChecks {
+class RegexpGenSpec extends WordSpec with MustMatchers with PropertyChecks {
 
   implicit val arbChar: Arbitrary[Char] = Arbitrary(Gen.alphaNumChar)
   implicit val noShrinkString: Shrink[String] = Shrink.shrinkAny
@@ -14,7 +14,7 @@ class RegexGenSpec extends WordSpec with MustMatchers with PropertyChecks {
     "create a valid generator from `gbp [+-]?[0-9]{5,9}\\.[0-9]{2}`" in {
 
       val r = "gbp [+-]?[0-9]{5,9}\\.[0-9]{2}"
-      val gen = RegexGen.from(r)
+      val gen = RegexpGen.from(r)
 
       forAll(gen) {
         str =>
@@ -26,7 +26,7 @@ class RegexGenSpec extends WordSpec with MustMatchers with PropertyChecks {
     "create a valid generator from `[1-9]\\d?(,\\d{3}){0,3}`" in {
 
       val r = "[1-9]\\d?(,\\d{3}){0,3}"
-      val gen = RegexGen.from(r)
+      val gen = RegexpGen.from(r)
 
       forAll(gen) {
         str =>
